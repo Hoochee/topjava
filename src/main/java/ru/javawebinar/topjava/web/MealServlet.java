59b873685f8e6461dbb0d90477a2dce9da00af73
Hoochee <jok777@yandex.ru>
2021-02-11 13:20:54 +0300

    2 1 HW1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,75 +1,22 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.dao.Dao;
-import ru.javawebinar.topjava.dao.MealDaoInMemory;
-import ru.javawebinar.topjava.model.Meal;
+import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = getLogger(Servlet.class);
-    private static Dao storage = new MealDaoInMemory();
-    private static final int CALORIES_PER_DAY = 2000;
-    private static String edit = "/editMealForm.jsp";
-    private static String meal = "/meals.jsp";
-
+    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        log.debug("redirect to meal");
-        req.setCharacterEncoding("UTF-8");
-        String forward = "";
-        String action = req.getParameter("action");
-        if (action == null) {
-            forward = meal;
-            req.setAttribute("list", MealsUtil.filteredByStreams(storage.getListOfMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY));
-        } else if (action.equalsIgnoreCase("delete")) {
-            int userId = Integer.parseInt(req.getParameter("userId"));
-            storage.delete(userId);
-            res.sendRedirect("meals");
-            req.setAttribute("list", MealsUtil.filteredByStreams(storage.getListOfMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY));
-            return;
-        } else if (action.equalsIgnoreCase("edit")) {
-            forward = edit;
-            int userId = Integer.parseInt(req.getParameter("userId"));
-            Meal meal = storage.getMeal(userId);
-            req.setAttribute("meal", meal);
-        } else {
-            forward = edit;
-        }
-        req.getRequestDispatcher(forward).forward(req, res);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        String stringID = req.getParameter("ID");
-        String description = req.getParameter("nameMeal");
-        int calories = Integer.parseInt(req.getParameter("calories"));
-        String stringDate = req.getParameter("date");
-        LocalDateTime datetime = LocalDateTime.parse(stringDate);
-        if (stringID != null) {
-            int id = Integer.parseInt(stringID);
-            storage.updateMeal(id, datetime, description, calories);
-            req.setAttribute("list", MealsUtil.filteredByStreams(storage.getListOfMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY));
-            req.getRequestDispatcher(meal).forward(req, resp);
-        } else {
-            storage.createMeal(datetime, description, calories);
-            resp.sendRedirect("meals");
-            req.setAttribute("list", MealsUtil.filteredByStreams(storage.getListOfMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY));
-            return;
-        }
-        req.getRequestDispatcher(meal).forward(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("getAll");
+        request.setAttribute("meals", MealsUtil.getTos(MealsUtil.meals, MealsUtil.DEFAULT_CALORIES_PER_DAY));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,34 +1,48 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-
+<%@ taglib prefix="fn" uri="http://topjava.javawebinar.ru/functions" %>
+<%--<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>--%>
 <html>
 <head>
-    <title>Meals</title>
+    <title>Meal list</title>
+    <style>
+        .normal {
+            color: green;
+        }
+
+        .excess {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-<h3><a href="index.html">Home</a></h3>
-<h2>Meals</h2>
-<table border="5">
-    <tr align="center">
-        <td style="display:none;">ID</td>
-        <td>Date</td>
-        <td>Description</td>
-        <td>Calories</td>
-        <td>Update</td>
-        <td>Delete</td>
-    </tr>
-    <c:forEach var="meal" items="${list}">
-        <tr align="center" bgcolor="${meal.excess?'red':'green'}">
-            <td style="display:none;">${meal.id }</td>
-            <td>${meal.formattedDateTime}</td>
-            <td>${meal.description}</td>
-            <td>${meal.calories}</td>
-            <td><a href="meals?action=edit&userId=<c:out value="${meal.id}"/>">Update</a></td>
-            <td><a href="meals?action=delete&userId=<c:out value="${meal.id}"/>">Delete</a></td>
+<section>
+    <h3><a href="index.html">Home</a></h3>
+    <hr/>
+    <h2>Meals</h2>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Calories</th>
         </tr>
-    </c:forEach>
-</table>
-<p><a href="meals?action=insert">Add Meal</a></p>
+        </thead>
+        <c:forEach items="${meals}" var="meal">
+            <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.MealTo"/>
+            <tr class="${meal.excess ? 'excess' : 'normal'}">
+                <td>
+                        <%--${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}--%>
+                        <%--<%=TimeUtil.toString(meal.getDateTime())%>--%>
+                        <%--${fn:replace(meal.dateTime, 'T', ' ')}--%>
+                        ${fn:formatDateTime(meal.dateTime)}
+                </td>
+                <td>${meal.description}</td>
+                <td>${meal.calories}</td>
+            </tr>
+        </c:forEach>
+    </table>
+</section>
 </body>
 </html>
